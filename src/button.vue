3f019940bced30fac6ace98fb562638395d8a57b
yezhangli <yezhangli@hzdracom.com>
2018-07-30 16:54:43 +0800

    加入按钮组件icon

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,24 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPositon}`]:true}">
+        <g-icon v-if="icon" :name="icon"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
     export default{
-
+        props:{
+            icon:{},
+            iconPositon:{
+                type:String,
+                default:'left',
+                validator(value){
+                    //return !(value !== 'left' && value !== 'right');
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 <style lang="scss">
@@ -14,6 +29,10 @@
         border-radius: var(--border-radius);
         border:1px solid var( --border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        vertical-align: top;
         &:hover{
             border-color:var(--border-color-hover) ;
         }
@@ -22,6 +41,23 @@
         }
         &:focus{
             outline:none;
+        }
+        >.content{
+            order:2;
+        }
+        >.g-icon{
+            order:1;
+            margin-right:.3em;
+        }
+        &.icon-right{
+            >.content{
+                order:1;
+            }
+            >.g-icon{
+                order:2;
+                margin-right:0;
+                margin-left: .3em;
+            }
         }
     }
 
